5483b0ae05f774629c5ae6ae65793de5f00269cc
Lucien Morey <lucienp@live.com.au>
2023-03-31 09:51:48 +1100

    hardware implementation of state feedback controller and kalman filter

--- ball_and_beam_controller/ball_and_beam_controller.ino
+++ ball_and_beam_controller/ball_and_beam_controller.ino
@@ -20,14 +20,22 @@
     BasicLinearAlgebra
  *******************************************************/
 
-IntervalTimer myTimer;
 #include <BasicLinearAlgebra.h>
 #include <ElementStorage.h>
 
 #include "kalman_filter.hpp"
 #include "state_feedback_controller.hpp"
+#include "conversions.h"
+#include "controller_state.h"
 
-#define fs 200 // Sampling frequency [Hz] , Ts = 1/fs [s]
+#define fs 1000                 // Sampling frequency [Hz] , Ts = 1/fs [s]
+float Ts = 1 / float(fs);       // Sampling Time Ts=1/fs [s] in seconds
+int Ts_m = (int)(Ts * 1000000); // Must multiply Ts by 1000000!
+
+BLA::Matrix<4, 4> EYE_4 = {1.0, 0.0, 0.0, 0.0,
+                           0.0, 1.0, 0.0, 0.0,
+                           0.0, 0.0, 1.0, 0.0,
+                           0.0, 0.0, 0.0, 1.0};
 
 // Input Pins
 // DO NOT CHANGE!!
@@ -44,16 +52,11 @@ IntervalTimer myTimer;
 #define OUT4 5 // Output 4 Pin
 
 // uC Inputs
-float in1 = 0; // Board Input 1 (0->12V)
-float in2 = 0; // Board Input 2 (0->12V)
-float in3 = 0; // Board Input 3 (+/-12V)
-float in4 = 0; // Board Input 4 (+/-12V)
+uint16_t in3 = 0; // Board Input 3 (+/-12V)
+uint16_t in4 = 0; // Board Input 4 (+/-12V)
 
 // uC Outputs
-float out1 = 0; // Board Output 1 (0->12V)
-float out2 = 0; // Board Output 2 (0->12V)
-float out3 = 0; // Board Output 3 (+/-12V)
-float out4 = 0; // Board Output 4 (+/-12V)
+uint16_t out4 = 0; // Board Output 4 (+/-12V)
 
 // Set Default Resolution of PWM Signal
  // ADC and PWM/DAC Resolution                                                      \
@@ -65,29 +68,17 @@ float out4 = 0; // Board Output 4 (+/-12V)
     Default resolution if not used is 10bits.
 const int res = 12;
 
-// Continuous State Space matrices
-BLA::Matrix<4, 4> Ac;
-BLA::Matrix<4, 1> Bc;
-BLA::Matrix<2, 4> Cc;
-BLA::Matrix<1, 1> Dc;
-
-// Discrete State Space matrices
-BLA::Matrix<4, 4> A;
-BLA::Matrix<4, 1> B;
-BLA::Matrix<2, 4> C;
-BLA::Matrix<1, 1> D;
-
+// forward_dec vars
 BLA::Matrix<2, 1> z_k;
 BLA::Matrix<4, 1> x_hat_k;
-BLA::Matrix<4, 4> P_k;
 
-BLA::Matrix<4, 1> x_ref;
-double u_ref;
+// Controller and Observer class instances
+KalmanFilter *kalman_filter;
+StateFeedbackController *state_feedback_controller;
 
-BLA::Matrix<4, 4> kalman_Q;
-BLA::Matrix<2, 2> kalman_R;
-
-BLA::Matrix<1, 4> K_SFC;
+ControllerState last_controller_state = STOPPED;
+ControllerState current_controller_state = STOPPED;
+IntervalTimer myTimer;
 
 // System Paramaters
 const double length = 0.91;                     // m
@@ -96,17 +87,55 @@ const double m = 0.03299;                       // kg
 const double r = 0.01;                          // m
 const double g = 9.81;                          // m/s^2
 const double J_b = 2.0 / 5.0 * m * pow(r, 2.0); // kg*m^2
-const double a = -13.3794;                      // TODO FIND THIS
-const double b = 747.4732;                      // TODO FIND THIS
+// const double a = -13.3794;                      // TODO FIND THIS
+// const double b = 747.4732;                      // TODO FIND THIS
+const double a = ;
+const double b = 0.002;
 
-const double position_noise = 0.1;
-const double angle_noise = 0.1;
-const double voltage_noise = 1e-6;
+// Continuous State Space matrices
+BLA::Matrix<4, 4> Ac = {0.0, 1.0, 0.0, 0.0,
+                        0.0, 0.0, -(m *g) / ((J_b / pow(r, 2.0)) + m), 0.0,
+                        0.0, 0.0, 0.0, 1.0,
+                        a, 0.0, 0.0, 0};
 
-// Controller and Observer class instances
+BLA::Matrix<4, 1> Bc = {0.0, 0.0, 0.0, b};
+BLA::Matrix<2, 4> Cc = {1.0, 0.0, 0.0, 0.0,
+                        0.0, 0.0, 1.0, 0.0};
+BLA::Matrix<1, 1> Dc = {0.0};
 
-KalmanFilter *kalman_filter;
-StateFeedbackController *state_feedback_controller;
+// Discrete State Space matrices
+BLA::Matrix<4, 4> A = EYE_4 - Ac * Ts;
+BLA::Matrix<4, 1> B = Bc * Ts;
+BLA::Matrix<2, 4> C = Cc;
+BLA::Matrix<1, 1> D = Dc;
+
+// Controller and Observer Config
+// State feedback gain
+BLA::Matrix<1, 4> K_SFC = {
+    -0.0838*1e4,   -0.1876*1e4,    1.0233*1e4,    0.3795*1e4
+};
+
+// Controller reference
+BLA::Matrix<4, 1> x_ref = {0.0, 0.0, 0.0, 0.0};
+double u_ref = 0.0;
+
+// hardware noise to create covariance matrices
+const double position_variance = 1.1212 / 100.0;
+const double angle_variance = 0.0045;
+const double voltage_variance = 1e-6;
+
+BLA::Matrix<4, 4> kalman_Q = EYE_4 * 1e5;
+BLA::Matrix<2, 2> kalman_R = {pow(position_variance, 2.0), 0,
+                              0, pow(angle_variance, 2.0)};
+
+// initial conditions
+// initial observer covariance
+BLA::Matrix<4, 4> P_0 = {1.0, 0.0, 0.0, 0.0,
+                         0.0, 1.0, 0.0, 0.0,
+                         0.0, 0.0, 1.0, 0.0,
+                         0.0, 0.0, 0.0, 1.0};
+//  initial observer estimate
+BLA::Matrix<4, 1> x_hat_0 = {0.0, 0.0, 0.0, 0.0};
 
 //___________________________________________________________________________
 //
@@ -120,42 +149,9 @@ void setup()
   Serial.begin(115200);
   delay(200);
 
-  // Sampling Time Ts=1/fs [s]
-  float Ts = 1 / float(fs); // in seconds
+  x_hat_k = x_hat_0;
 
-  // Set up continuous time matrices
-  Ac = {0.0, 1.0, 0.0, 0.0,
-        0.0, 0.0, -(m * g) / ((J_b / pow(r, 2.0)) + m), 0.0,
-        0.0, 0.0, 0.0, 1.0,
-        0.0, 0.0, 0.0, a};
-
-  Bc = {0.0, 0.0, 0.0, b};
-
-  Cc = {1.0, 0.0, 0.0, 0.0,
-        0.0, 0.0, 1.0, 0.0};
-
-  Dc = {0.0};
-
-  // Discretise continuous time matrices
-  BLA::Matrix<4, 4> eye_4 = {1.0, 0.0, 0.0, 0.0,
-                             0.0, 1.0, 0.0, 0.0,
-                             0.0, 0.0, 1.0, 0.0,
-                             0.0, 0.0, 0.0, 1.0};
-  A = eye_4 - Ac * Ts;
-  B = Bc * Ts;
-  C = Cc;
-  D = Dc;
-
-  K_SFC = {-0.0425, -0.0398, 0.1923, 0.0122};
-
-  kalman_Q = eye_4 * 1e5;
-  kalman_R = {pow(position_noise, 2.0), 0,
-              0, pow(angle_noise, 2.0)};
-
-  x_hat_k = {0.0, 0.0, 0.0, 0.0};
-  P_k = eye_4;
-
-  kalman_filter = new KalmanFilter(A, B, C, kalman_Q, kalman_R, x_hat_k, P_k);
+  kalman_filter = new KalmanFilter(A, B, C, kalman_Q, kalman_R, x_hat_0, P_0);
   state_feedback_controller = new StateFeedbackController(K_SFC);
 
   // Initialize I/O pins to measure execution time
@@ -183,10 +179,6 @@ void setup()
   analogWriteFrequency(OUT2, 100000);
   analogWriteFrequency(OUT3, 100000);
   analogWriteFrequency(OUT4, 100000);
-
-  // Create an IntervalTimer object
-  int Ts_m = (int)(Ts * 1000000); // Must multiply Ts by 1000000!
-  myTimer.begin(Controller, Ts_m);
 }
 
 //___________________________________________________________________________
@@ -200,55 +192,42 @@ void Controller(void)
 {
 
   // The code inside this section will be run at every Ts
-
   // Start measuring execution time
   digitalWrite(A3, HIGH);
 
   /*
   Board Inputs
   */
-
   // read_inputx(disp) //disp = 1 will display input data in Serial Monitor
-  in1 = read_input1(0); // 0 -> 12v
-  in2 = read_input2(0); // 0 -> 12v
-  in3 = read_input3(0); // -12v -> 12v
-  in4 = read_input4(0); // -12v -> 12v
-
-  // disp_inputs_all(); // Displays all inputs
-
-  // Only display inputs for calibration.
-  // Do not display them when running the controller
+  in3 = analogRead(IN3); // -12v -> 12v
+  in4 = analogRead(IN4); // -12v -> 12v
 
   // Discrete-time Controller
-
-  /* EXAMPLE - YOU MUST CHANGE */
-
   // Output Measurement
-  z_k = {in1,
-         in2};
+  z_k = {adcToBallPosition(in4), adcToBeamAngleRads(in3)};
 
   // Control Algorithim
   double u_k = state_feedback_controller->compute_control_input(u_ref, x_ref, x_hat_k);
-
+  // Map Contol Effort to output
+  out4 = driveVoltageToDAC(u_k);
+  // Update state estimate
   x_hat_k = kalman_filter->filter(u_k, z_k);
 
-  // Map Contol Effort to output
-  out1 = u_k;
+  // debugging prints
+  // Serial.printf("BALL POS %f, ANGLE %f\n", adcToBallPosition(in4), adcToBeamAngleDegrees(in3));
+  auto last_in = kalman_filter->get_last_innovation();
+  // Serial.printf("Last innovation );
+  Serial.printf("x_hat_k, %f, %f, %f, %f, u_k %f, position_inno %f, angle inno %f\n",
+                x_hat_k(0, 0), x_hat_k(1, 0), x_hat_k(2, 0) * 180 / M_PI,
+                x_hat_k(3, 0) * 180 / M_PI, u_k, last_in(0, 0), last_in(1, 0));
+
+  // Serial.printf("Drive Voltage %f\n", u_k);
 
   /*
   Board Outputs
   */
-  // disp = 1 will display
-  // individual output in serial monitor
-
   // write_outx(value, disp)
-  write_out1(out1, 0);
-  write_out2(out2, 0);
-  write_out3(out3, 0);
-  write_out4(out4, 0);
-  // disp_outputs_all();
-  // Only display outputs for calibration.
-  // Do not display them when running the controller
+  analogWrite(OUT4, out4);
 
   // Stop measuring calculation time
   digitalWrite(A3, LOW);
@@ -260,301 +239,57 @@ void Controller(void)
 //
 //              Does nothin - main loop left intentionally empty!
 //___________________________________________________________________________
-void loop() {}
-
-//___________________________________________________________________________
-//
-//                              disp_inputs_all
-//
-//                Displays all inputs on serial monitor
-//___________________________________________________________________________
-void disp_inputs_all()
+void loop()
 {
-  Serial.print("In1: ");
-  Serial.print(in1, 3);
-  Serial.print(" [V]  ");
-  Serial.print("In2: ");
-  Serial.print(in2, 3);
-  Serial.print(" [V]  ");
-  Serial.print("In3: ");
-  Serial.print(in3, 3);
-  Serial.print(" [V]  ");
-  Serial.print("In4: ");
-  Serial.print(in4, 3);
-  Serial.println(" [V]");
-}
-
-//___________________________________________________________________________
-//                              disp_outputs_all
-//
-//              Displays all outputs on serial monitor
-//___________________________________________________________________________
-void disp_outputs_all()
-{
-  Serial.print("Out1: ");
-  Serial.print(out1);
-  Serial.print(" [V]  ");
-  Serial.print("Out2: ");
-  Serial.print(out2);
-  Serial.print(" [V]  ");
-  Serial.print("Out3: ");
-  Serial.print(out3);
-  Serial.print(" [V]  ");
-  Serial.print("Out4: ");
-  Serial.print(out4);
-  Serial.print(" [V]  ");
-}
-
-//___________________________________________________________________________
-//                              read_inputx
-//
-//        Reads respective analog pin and returns an actual voltage.
-//        The raw bits must be scaled!
-//___________________________________________________________________________
-
-float read_input1(int disp)
-{
-  float in_float = 0;
-
-  int ADC_raw = analogRead(IN1); // Read input 1 (0 –> 12V)
-
-  in_float = (float)(ADC_raw);
-
-  // convert ADC_raw to voltage
-  //  Here, you must enter the experimental linear relationship
-  //  between the raw bits and the actual voltage.
-  //  acutal voltage = in_float*m +/-
-  float p1 = 1;
-  float p2 = 0;
-  in_float = in_float * p1 + p2;
-
-  if (disp == 1)
+  if (Serial.available() > 0)
   {
-    Serial.print("In1: ");
-    Serial.print(in_float);
-    Serial.println(" [V]");
+    int user_input = Serial.read();
+
+    switch (user_input)
+    {
+    case 's':
+      // Stop mode
+      current_controller_state = STOPPED;
+      Serial.println("Switching to Stopped");
+      break;
+    case 'c':
+      // control mode
+      current_controller_state = CONTROLLING;
+      Serial.println("Switching to Controlling");
+      break;
+    case 'm':
+      // manual mode
+      current_controller_state = MANUAL;
+      Serial.println("Switching to Manual");
+    default:
+      Serial.println("input to change mode invalid");
+    }
+  }
+  // flush buffer
+  Serial.flush();
+
+  if (current_controller_state != last_controller_state)
+  {
+    if ((current_controller_state == CONTROLLING) && (last_controller_state != CONTROLLING))
+    {
+      myTimer.begin(Controller, Ts_m);
+    }
+    else
+    {
+      myTimer.end();
+      double u_k = 0.0;
+      analogWrite(OUT4, driveVoltageToDAC(u_k));
+    }
   }
 
-  return in_float;
-}
-
-float read_input2(int disp)
-{
-  float in_float = 0;
-
-  int ADC_raw = analogRead(IN2); // Read input 2 (0 –> 12V)
-
-  in_float = (float)(ADC_raw);
-
-  // convert ADC_raw to voltage
-  //  Here, you must enter the experimental linear relationship
-  //  between the raw bits and the actual voltage.
-  //  acutal voltage = in_float*m +/-
-  float p1 = 1;
-  float p2 = 0;
-  in_float = in_float * p1 + p2;
-
-  if (disp == 1)
+  if (current_controller_state != CONTROLLING)
   {
-    Serial.print("In1: ");
-    Serial.print(in_float);
-    Serial.println(" [V]");
+    in3 = analogRead(IN3); // -12v -> 12v
+    in4 = analogRead(IN4); // -12v -> 12v
+    double u_k = 0.0;
+    analogWrite(OUT4, driveVoltageToDAC(u_k));
+    Serial.printf("BALL POSITION %f, BALL_ADC %d, BEAM ANGLE %f, ANGLE ADC %d, DAC OUTPUT %d\n", adcToBallPosition(in4), in4, adcToBeamAngleDegrees(in3), in3, driveVoltageToDAC(u_k));
   }
 
-  return in_float;
-}
-
-float read_input3(int disp)
-{
-  float in_float = 0;
-
-  int ADC_raw = analogRead(IN3); // Read input 3 (0 -> +/-12)
-
-  in_float = (float)(ADC_raw);
-
-  // convert ADC_raw to voltage
-  //  Here, you must enter the experimental linear relationship
-  //  between the raw bits and the actual voltage.
-  //  acutal voltage = in_float*m +/-
-  float p1 = 1;
-  float p2 = 0;
-  in_float = in_float * p1 + p2;
-
-  if (disp == 1)
-  {
-    Serial.print("In1: ");
-    Serial.print(in_float);
-    Serial.println(" [V]");
-  }
-
-  return in_float;
-}
-
-float read_input4(int disp)
-{
-  float in_float = 1;
-
-  int ADC_raw = analogRead(IN4); // Read input 4 (0 -> +/-12)
-
-  in_float = (float)(ADC_raw);
-
-  // convert ADC_raw to voltage
-  //  Here, you must enter the experimental linear relationship
-  //  between the raw bits and the actual voltage.
-  //  acutal voltage = in_float*m +/-
-  float p1 = 1;
-  float p2 = 0;
-  in_float = in_float * p1 + p2;
-
-  if (disp == 1)
-  {
-    Serial.print("In1: ");
-    Serial.print(in_float);
-    Serial.println(" [V]");
-  }
-
-  return in_float;
-}
-
-//___________________________________________________________________________
-//                              write_outx
-//
-//        Given a desired output value, it will scale this to an output
-//        value for DAC.
-//        The raw bits must be scaled!
-//___________________________________________________________________________
-void write_out1(float out, int disp)
-{
-  //  Here, you must enter the experimental linear relationship
-  //  between duty cycle d and the actual voltage.
-  //  d = out*p1 +/- p2
-  float d;
-  float p1 = 1;
-  float p2 = 0;
-  d = out * p1 + p2;
-
-  if (d < 0)
-  {
-    d = 0;
-  }
-  if (d > 1)
-  {
-    d = 1;
-  }
-
-  // Convert D to A
-  int DAC_raw = (int)(d * 4095);
-
-  // Write to output
-  analogWrite(OUT1, DAC_raw);
-
-  // Serial Montior/Plotter
-  if (disp == 1)
-  {
-    Serial.print("Out1 : ");
-    Serial.print(out, 10);
-    Serial.println(" [V]");
-  }
-}
-
-void write_out2(float out, int disp)
-{
-  //  Here, you must enter the experimental linear relationship
-  //  between duty cycle d and the actual voltage.
-  //  d = out*p1 +/- p2
-  float d;
-  float p1 = 1;
-  float p2 = 0;
-  d = out * p1 + p2;
-
-  if (d < 0)
-  {
-    d = 0;
-  }
-  if (d > 1)
-  {
-    d = 1;
-  }
-
-  // Convert D to A
-  int DAC_raw = (int)(d * 4095);
-
-  // Write to output
-  analogWrite(OUT2, DAC_raw);
-
-  // Serial Montior/Plotter
-  if (disp == 1)
-  {
-    Serial.print("Out2 : ");
-    Serial.print(out);
-    Serial.println(" [V]");
-  }
-}
-
-void write_out3(float out, int disp)
-{
-  //  Here, you must enter the experimental linear relationship
-  //  between duty cycle d and the actual voltage.
-  //  d = out*p1 +/- p2
-  float d;
-  float p1 = 1;
-  float p2 = 0;
-  d = out * p1 + p2;
-
-  if (d < 0)
-  {
-    d = 0;
-  }
-  if (d > 1)
-  {
-    d = 1;
-  }
-
-  // Convert D to A
-  int DAC_raw = (int)(d * 4095);
-
-  // Write to output
-  analogWrite(OUT3, DAC_raw);
-
-  // Serial Montior/Plotter
-  if (disp == 1)
-  {
-    Serial.print("Out3 : ");
-    Serial.print(out);
-    Serial.println(" [V]");
-  }
-}
-
-void write_out4(float out, int disp)
-{
-  //  Here, you must enter the experimental linear relationship
-  //  between duty cycle d and the actual voltage.
-  //  d = out*p1 +/- p2
-  float d;
-  float p1 = 1;
-  float p2 = 0;
-  d = out * p1 + p2;
-
-  if (d < 0)
-  {
-    d = 0;
-  }
-  if (d > 1)
-  {
-    d = 1;
-  }
-
-  // Convert D to A
-  int DAC_raw = (int)(d * 4095);
-
-  // Write to output
-  analogWrite(OUT4, DAC_raw);
-
-  // Serial Montior/Plotter
-  if (disp == 1)
-  {
-    Serial.print("Out4 : ");
-    Serial.print(out);
-    Serial.println(" [V]");
-  }
+  last_controller_state = current_controller_state;
 }
