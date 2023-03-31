5eab90c81f464869c08f120fc1da50698d9576ba
Lucien Morey <lucienp@live.com.au>
2023-03-31 18:46:36 +1100

    fix motor dynamics func

--- grey_box_id/motor_dynamics.m
+++ grey_box_id/motor_dynamics.m
@@ -1,12 +1,13 @@
 function [A,B,C,D] = motor_dynamics(k_theta, k_theta_dot, k_v, Ts)
 
-A = [0, 0;
+%disp(k_theta, k_theta_dot, k_v);
+A = [0, 1;
      k_theta, k_theta_dot];
 
 B = [0;
      k_v];
 
-C = [1, 0];
+C = [0, 1];
 
 D = 0;
 end
