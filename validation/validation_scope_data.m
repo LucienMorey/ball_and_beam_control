2b59b297fb923b85d505579c77fa12641a7e5f03
George <gsheslow@yahoo.com.au>
2023-04-06 09:15:13 +1000

    validation/scope_3

--- validation/validation_scope_data.m
+++ validation/validation_scope_data.m
@@ -4,11 +4,13 @@ clc
 data = readtable("scope_4.csv");
 data = data(3:end-1,:);
 
+data.Var3 = movmean(data.Var3,20);
+
 ts = data.Var1(2) - data.Var1(2);
 
-inital_pos = mean(data.Var3(1:10))
+inital_pos = mean(data.Var3(1:5))
 
-finish_pos = mean(data.Var3(end-10:end))
+finish_pos = mean(data.Var3(end-5:end))
 
 stepinfo(data.Var3,data.Var1,finish_pos,inital_pos)
 
