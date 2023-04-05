c8fd9c2560b4cc5af0d7c916c5e752672d29c08b
George <gsheslow@yahoo.com.au>
2023-04-06 08:50:21 +1000

    Add validation files for closed loop system

---
+++ validation/validation_scope_data.m
@@ -0,0 +1,17 @@
+clear 
+clc
+
+data = readtable("scope_4.csv");
+data = data(3:end-1,:);
+
+ts = data.Var1(2) - data.Var1(2);
+
+inital_pos = mean(data.Var3(1:10))
+
+finish_pos = mean(data.Var3(end-10:end))
+
+stepinfo(data.Var3,data.Var1,finish_pos,inital_pos)
+
+figure(1)
+
+plot(data.Var1,data.Var3)
