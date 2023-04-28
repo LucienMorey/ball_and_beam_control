9e3498e7958d1a218370548cc27bcd92dfb562ac
Lucien Morey <lucienp@live.com.au>
2023-04-28 13:07:40 +1000

    add print out for verification

--- ball_and_beam_controller/ball_and_beam_controller.ino
+++ ball_and_beam_controller/ball_and_beam_controller.ino
@@ -251,6 +251,7 @@ void Controller(void)
   x_hat_k = kalman_filter->filter(u_k, z_k);
 
   std::cout << "u_k " << u_k.format(CleanFmt) << " pos & angle " << z_k.transpose().format(CleanFmt) << " x_hat " << x_hat_k.transpose().format(CleanFmt) << std::endl;
+  // std::cout << "last controller gain " << lqr_controller->get_last_gain_matrix().format(CleanFmt) << std::endl;
 
   // Board Outputs
   analogWrite(OUT4, out4);
