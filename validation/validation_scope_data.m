4f48c296e95bdb247d766c980a1990c5411ba7eb
George <gsheslow@yahoo.com.au>
2023-05-12 15:24:00 +1000

    Fix variable naming

--- validation/validation_scope_data.m
+++ validation/validation_scope_data.m
@@ -1,26 +1,30 @@
 clear 
 clc
 
-data = readtable("scope_4.csv");
+data = readtable("scope_44.csv");
 data = data(3:end-1,:);
 
-data.Var3 = movmean(data.Var3,20);
+% time
+time = data.Var1;
+
+% conversion
+ballPos = (16.838518 * data.Var3 + 2.763498)/100;
+
+% averaging/filtering
+ballPos = movmean(ballPos,20);
 
 ts = data.Var1(2) - data.Var1(2);
 
-inital_pos = mean(data.Var3(1:5))
+inital_pos = mean(ballPos(1:5));
+finish_pos = -0.2;
 
-% finish_pos = mean(data.Var3(end-5:end))
-
-finish_pos = -20;
-finish_pos = (finish_pos - 2.763498) /16.838518 %Ball Position = 16.838518 * Signal + 2.763498 
-stepinfo(data.Var3,data.Var1,finish_pos,inital_pos)
+stepinfo(ballPos,time,finish_pos,inital_pos)
 
 figure(1)
 
-plot(data.Var1,data.Var3)
+plot(time,ballPos)
 hold on
-plot(data.Var1, ones(size(data.Var1))*finish_pos)
+plot(time, ones(size(time))*finish_pos)
 title("Experimental Data Closed Loop Validation")
 ylabel("Ball Position (Voltage)")
 xlabel("Time (s)")
