d2074cf28d8a6b8fe0457da2f86f9a2da45a2a33
George <gsheslow@yahoo.com.au>
2023-05-18 13:11:16 +1000

    Add second plot, time response

--- stability_analysis/plots.m
+++ stability_analysis/plots.m
@@ -1,9 +1,10 @@
 %% 
-controller = 1; % 1: SFC
+controller = 2; % 1: SFC
                 % 2: LQR
                 % 3: SMC
 
-pos_ref = 0;
+pos_ref = 0.0;
+averaging = 50;
 %%
 
 figure(1)
@@ -35,14 +36,32 @@ for k=1:(size(Files,1)-2)
 
     % do conversion to real units
     
-%     ballPos = (16.838518 .* ballPos + 2.763498)/100;
+    ballPos = (16.838518 .* ballPos + 2.763498)/100;
     ballPos = ballPos - pos_ref;
-%     beamAngle = 1.040051 .* beamAngle + 0.696110;
+    beamAngle = 1.040051 .* beamAngle + 0.696110;
     
     % some filtering
     
-    ballPos = movmean(ballPos,30);
-    beamAngle = movmean(beamAngle,30);
+    ballPos = movmean(ballPos,averaging);
+    beamAngle = movmean(beamAngle,averaging);
     
     plot(ballPos,beamAngle)
 end
+
+%% Plot time response just for 1st dataset
+figure(2)
+ts = data.Var1(2) - data.Var1(1);
+time = [0:ts:ts*size(data.Var1,1)-ts]'
+subplot(2,1,1)
+plot(time,ballPos)
+hold on
+plot(time,beamAngle)
+if(controller == 1)
+    title("SFC with integral action",'fontsize',16,'interpreter','latex');
+elseif(controller == 2)
+    title("LQR with integral action",'fontsize',16,'interpreter','latex')
+elseif(controller == 3)
+    title("Sliding Mode Control",'fontsize',16,'interpreter','latex')
+end
+subplot(2,1,2)
+plot(time,data.Var2)
